73aa5a2a3dbd2c3fa1b99f5507ccf088193076de
Sarvadnya Yeole <yeolesv1012@gmail.com>
2025-09-18 22:13:53 +0530

    Set up all the required stuff for the project

---
+++ frontend/Project3/src/App.jsx
@@ -0,0 +1,19 @@
+import axios from "axios";
+import React, { useEffect } from "react";
+
+const App = () => {
+  const getproduct = async () => {
+    try {
+      const res = await axios.get("/products");
+      console.log(res);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getproduct();
+  });
+  return <div>App</div>;
+};
+
+export default App;
---
+++ frontend/Project3/src/Pageerror.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Pageerror = () => {
+  return (
+    <div className="text-center w-screen h-screen">
+      <h1 className="text-red-500 text-9xl">Page Not found</h1>
+    </div>
+  );
+};
+
+export default Pageerror;
