30a32c2bede4245fa4ff0210c285c0a233f270df
Sarvadnya Yeole <yeolesv1012@gmail.com>
2025-09-20 21:15:27 +0530

    dispatching asynccurrentuser action in App component.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Mainroutes from "./routes/Mainroutes";
 import Nav from "./components/Nav";
+import { asynccurrentuser } from "./store/actions/UserActions";
+import { useDispatch } from "react-redux";
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(asynccurrentuser());
+  }, []);
   return (
     <div className="bg-gray-800 w-screen h-screen text-white font-[gilroy]">
       <Nav />
@@ -9,5 +16,5 @@ const App = () => {
     </div>
   );
 };
- 
+
 export default App;
