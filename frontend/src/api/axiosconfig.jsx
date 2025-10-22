966c4c41fac73bf4d264b4088df889b798f0b706
Sarvadnya Yeole <yeolesv1012@gmail.com>
2025-10-22 10:54:42 +0530

    Added deployed json server url to axios

--- frontend/src/api/axiosconfig.jsx
+++ frontend/src/api/axiosconfig.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
 const instance = axios.create({
-  baseURL: "http://localhost:3000/",
+  baseURL: "https://e-commerce-project-9-drya.onrender.com",
 });
 export default instance;
